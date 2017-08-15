80f3470b23224c158946ccd4e7375edb6fd306b5
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-08-15 12:26:07 +0100

    Add saving note functionality and thorough tests for it

--- note-list-model.js
+++ note-list-model.js
@@ -10,3 +10,14 @@
 NoteList.prototype.getNotes = function() {
 	return this.notes;
 };
+
+NoteList.prototype.makeNote = function(text) {
+  note = new Note(text);
+  this._addNote(note);
+};
+
+// Private
+
+NoteList.prototype._addNote = function(note) {
+  this.notes.push(note);
+};
--- tests/note-list-model-test.js
+++ tests/note-list-model-test.js
@@ -1,12 +1,21 @@
+window.noteList = new NoteList();
+
 function listIsAnarray() {
-  var noteList = new NoteList();
+  // var noteList = new NoteList();
   assert.truthy(noteList.getNotes().constructor === Array);
 };
 
 function listHasNoNotesByDefault() {
-  var noteList = new NoteList();
+  // var noteList = new NoteList();
   assert.truthy(noteList.getNotes().length === 0);
+};
+
+function listCanCreateAndStoreNotes() {
+  text = "Hi, I am a note";
+  noteList.makeNote(text);
+  assert.truthy(noteList.getNotes().pop().getText() === text);
 };
 
 listIsAnarray();
 listHasNoNotesByDefault();
+listCanCreateAndStoreNotes();
